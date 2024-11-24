c58375a4f164442e18a7945ff502649b2a15e61a
Alon Ohana <alon.o@sealsecurity.io>
2024-11-24 10:49:53 +0200

    CVE-2024-52804

--- tornado/httputil.py
+++ tornado/httputil.py
@@ -1014,60 +1014,17 @@ def qs_to_qsl(qs):
         for v in vs:
             yield (k, v)
 
+_unquote_sub = re.compile(r"\\(?:([0-3][0-7][0-7])|(.))").sub
 
-_OctalPatt = re.compile(r"\\[0-3][0-7][0-7]")
-_QuotePatt = re.compile(r"[\\].")
-_nulljoin = ''.join
+def _unquote_replace(m: re.Match) -> str:
+    if m[1]:
+        return chr(int(m[1], 8))
+    else:
+        return m[2]
 
 
 def _unquote_cookie(str):
-    """Handle double quotes and escaping in cookie values.
-
-    This method is copied verbatim from the Python 3.5 standard
-    library (http.cookies._unquote) so we don't have to depend on
-    non-public interfaces.
-    """
-    # If there aren't any doublequotes,
-    # then there can't be any special characters.  See RFC 2109.
-    if str is None or len(str) < 2:
-        return str
-    if str[0] != '"' or str[-1] != '"':
-        return str
-
-    # We have to assume that we must decode this string.
-    # Down to work.
-
-    # Remove the "s
-    str = str[1:-1]
-
-    # Check for special sequences.  Examples:
-    #    \012 --> \n
-    #    \"   --> "
-    #
-    i = 0
-    n = len(str)
-    res = []
-    while 0 <= i < n:
-        o_match = _OctalPatt.search(str, i)
-        q_match = _QuotePatt.search(str, i)
-        if not o_match and not q_match:              # Neither matched
-            res.append(str[i:])
-            break
-        # else:
-        j = k = -1
-        if o_match:
-            j = o_match.start(0)
-        if q_match:
-            k = q_match.start(0)
-        if q_match and (not o_match or k < j):     # QuotePatt matched
-            res.append(str[i:k])
-            res.append(str[k + 1])
-            i = k + 2
-        else:                                      # OctalPatt matched
-            res.append(str[i:j])
-            res.append(chr(int(str[j + 1:j + 4], 8)))
-            i = j + 4
-    return _nulljoin(res)
+    return _unquote_sub(_unquote_replace, str)
 
 
 def parse_cookie(cookie):
